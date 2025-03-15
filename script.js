5cc8da50cf132a4ed3f659eb6c01a14f3de803e9
LEv421 <lev.marinchenko@gmail.com>
2025-03-15 13:22:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ document.getElementById("reload-btn").addEventListener("click", function () {
     document.body.style.background = "black";
     document.body.innerHTML = "";
 
-    // Создание нового элемента audio
+    // Создаем новый объект Audio
     let audio = new Audio("whatsapp-bass-boosted.mp3");
     audio.volume = 1.0; // Максимальная громкость
-
-    // Попробовать запустить звук после взаимодействия
-    audio.play().catch(error => {
-        console.error("Ошибка воспроизведения звука:", error);
-    });
+    audio.play().catch(error => console.error("Ошибка воспроизведения звука:", error));
 });
